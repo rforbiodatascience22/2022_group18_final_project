1a0aed3123c02a7877462cd0c8bc4df3d5394944
pokepup <s213012@dtu.dk>
2022-04-15 16:09:17 +0000

    removed comment

--- PancRISK_project/R/01_load.R
+++ PancRISK_project/R/01_load.R
@@ -5,7 +5,6 @@ library("tidyverse")
 # Load data ---------------------------------------------------------------
 my_data_raw <- read_csv(file = "PancRISK_project/data/_raw/PancRISK_raw.csv", 
                         na = c("", "NA"))
-# Optional to drop NAs of plasma_CA19_9, since it is important to compare with urinary values.
 
 
 # Write data --------------------------------------------------------------
