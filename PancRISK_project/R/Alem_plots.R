ac5fb018a25d445c3b54bc1e1e458bbed1678489
pokepup <s213012@dtu.dk>
2022-04-20 13:52:33 +0000

    Plots updates

--- PancRISK_project/R/Alem_plots.R
+++ PancRISK_project/R/Alem_plots.R
@@ -1,15 +1,28 @@
 library("tidyverse")
 data_long <- my_data_clean %>% 
-  select(patient_cohort, plasma_CA19_9, creatinine, LYVE1, REG1A, REG1B, TFF1) %>% 
-  pivot_longer(!patient_cohort, names_to = "protein", values_to = "expression")
+  select(patient_cohort, 
+         plasma_CA19_9, 
+         creatinine, 
+         LYVE1, 
+         REG1A, 
+         REG1B, 
+         TFF1) %>% 
+  pivot_longer(!patient_cohort, 
+               names_to = "protein", 
+               values_to = "expression")
 
 data_long %>%
   group_by(patient_cohort) %>%
-  ggplot(mapping = aes(y = patient_cohort, fill = patient_cohort)) +
-  facet_wrap(~protein, ncol = 2, scales = "free") +
+  ggplot(mapping = aes(y = patient_cohort, 
+                       fill = patient_cohort)) +
+  facet_wrap(~protein, 
+             ncol = 2, 
+             scales = "free") +
   geom_bar() +
   xlab("Occurence of measured protein levels per Cohort ") +
   ylab("Cohort of patients") +
+  labs(title = "Sample distribution between two cohorts of patients") +
   theme_minimal() +
   theme(legend.position="none")
+  
  
