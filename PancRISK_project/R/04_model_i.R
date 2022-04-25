96ef173aea650d6d298bc39ac975cd9044e3fda9
Your Name <you@example.com>
2022-04-25 06:38:03 +0000

    Make code more tidy

--- PancRISK_project/R/04_model_i.R
+++ PancRISK_project/R/04_model_i.R
@@ -1,5 +1,3 @@
-library("broom")
-library("cowplot")
 library("scales")
 library("nnet")
 library("pROC")
@@ -10,21 +8,24 @@ library("patchwork")
 library("kableExtra")
 library("cutoff")
 
+validate <- function(data, model, diagnosis_1, diagnosis_2) {
+  
+}
+
 find_cutoff <- function(data, const_1, const_2) {
   cutoff_val <- cutoff::roc(data$pred, data$diagnosis)$cutoff
   
-  data <-
-    data %>%
+  data %>%
     mutate(pred = ifelse(pred < cutoff_val, const_1, const_2))
 }
 
-fit <- function(data, form, contrast) {
+fit <- function(data, form, contrast_1, contrast_2) {
   data %>%
-  filter(diagnosis == contrast[1] | diagnosis == contrast[2]) %>%
-  glm(formula = form,
-      data = .,
-      na.action = na.omit,
-      family = binomial(link = "logit"))
+    filter(diagnosis == contrast_1 | diagnosis == contrast_2) %>%
+    glm(formula = form,
+        data = .,
+        na.action = na.omit,
+        family = binomial(link = "logit"))
 }
 
 print_acc <- function(data_1, data_2, title = "Group") {
@@ -54,7 +55,8 @@ print_acc <- function(data_1, data_2, title = "Group") {
 
   data_2 %>%
     tidy() %>%
-    pivot_wider(values_from = estimate, names_from = class) %>%
+    pivot_wider(values_from = estimate, 
+                names_from = class) %>%
     filter(term == "sensitivity" | term == "specificity") %>%
     select(c(term, '2')) %>%
     mutate_at(c('2'), round, 3) %>%
@@ -67,9 +69,14 @@ print_acc <- function(data_1, data_2, title = "Group") {
     add_header_above(c("Accurancy\na" = 1, setNames(1, accurancy_1), setNames(1, accurancy_2)), line = TRUE, bold = FALSE) %>%
     add_header_above(c(setNames(3, paste("Performance - ", title)))) %>% 
     kable_styling(position = "center") %>%
-    column_spec (1:3, border_left = "1px solid #ddd;", border_right = "1px solid #ddd;") %>%
-    row_spec(row = 1:2, font_size = 24, hline_after = "1px solid #ddd;") %>%
-    column_spec(column = 1, width = "6em")
+    column_spec (1:3, 
+                 border_left = "1px solid #ddd;", 
+                 border_right = "1px solid #ddd;") %>%
+    row_spec(row = 1:2, 
+             font_size = 24, 
+             hline_after = "1px solid #ddd;") %>%
+    column_spec(column = 1, 
+                width = "6em")
 }
 
 plot_roc <- function(roc_render_1, roc_render_2, title = "ROC") {
@@ -86,9 +93,16 @@ plot_roc <- function(roc_render_1, roc_render_2, title = "ROC") {
     ci.auc() %>%
     round(3)
   ggplot(NULL) +
-    geom_segment(aes(x = 0, y = 1, xend = 1, yend = 0), alpha = 0.5) + 
-    geom_line(aes(x = plotx_1, y = ploty_1, color = "low")) +
-    geom_line(aes(x = plotx_2, y = ploty_2, color = "high")) +
+    geom_segment(aes(x = 0, 
+                     y = 1, 
+                     xend = 1, 
+                     yend = 0), alpha = 0.5) + 
+    geom_line(aes(x = plotx_1, 
+                  y = ploty_1, 
+                  color = "low")) +
+    geom_line(aes(x = plotx_2, 
+                  y = ploty_2, 
+                  color = "high")) +
     scale_color_discrete(name="Contrast",
                          labels=c(paste0("Control vs. Benign\n",
                                          round(auc_1, 3), ", 95% CI: ", auc_ci_1[1], "-", auc_ci_1[3]), 
@@ -160,11 +174,6 @@ my_data_test <-
   my_data_clean %>% 
   setdiff(my_data_train)
 
-## Plasma
-
-
-## PancRISK
-form_risk = formula(diagnosis ~  REG1B + LYVE1 + TFF1 + cutoff_plasma + age + creatinine)
 pancrisk_model_plasma <- 
   my_data_train %>%
   multinom(formula = diagnosis ~ REG1B + LYVE1 + TFF1 + cutoff_plasma + age + creatinine,
@@ -172,46 +181,45 @@ pancrisk_model_plasma <-
            model = TRUE,
            na.action = na.omit)
 
-pancrisk_model_1 <- fit(my_data_train, 
-                        formula(diagnosis ~ REG1B + LYVE1 + TFF1 + age + creatinine), 
-                        contrast = c("control", "benign"))
+pancrisk_model_1 <- fit(data = my_data_train, 
+                        form = formula(diagnosis ~ REG1B + LYVE1 + TFF1 + age + creatinine), 
+                        contrast_1 = "control", 
+                        contrast_2 = "benign")
 
-pancrisk_model_2 <- fit(my_data_train, 
-                        formula(diagnosis ~ REG1B + LYVE1 + TFF1 + age + creatinine), 
-                        contrast = c("control", "malignant"))
+pancrisk_model_2 <- fit(data = my_data_train, 
+                        form = formula(diagnosis ~ REG1B + LYVE1 + TFF1 + age + creatinine), 
+                        contrast_1 = "control", 
+                        contrast_2 = "malignant")
 
-pancrisk_simple_model_1 <- fit(my_data_train, 
-                               formula(diagnosis ~ REG1A + LYVE1 + TFF1 + age + creatinine), 
-                               contrast = c("control", "benign"))
+pancrisk_simple_model_1 <- fit(data = my_data_train, 
+                               form = formula(diagnosis ~ REG1A + LYVE1 + TFF1 + age + creatinine), 
+                               contrast_1 = "control", 
+                               contrast_2 = "benign")
 
-pancrisk_simple_model_2 <- fit(my_data_train, 
-                               formula(diagnosis ~ REG1A + LYVE1 + TFF1 + age + creatinine), 
-                               contrast = c("control", "malignant"))
+pancrisk_simple_model_2 <- fit(data = my_data_train, 
+                               form = formula(diagnosis ~ REG1A + LYVE1 + TFF1 + age + creatinine), 
+                               contrast_1 = "control", 
+                               contrast_2 = "malignant")
 
-pancrisk_just_plasma_model_1 <- fit(my_data_plasma, 
-                                    formula(diagnosis ~ cutoff_plasma + age), 
-                                    contrast = c("control", "benign"))
+pancrisk_just_plasma_model_1 <- fit(data = my_data_plasma, 
+                                    form = formula(diagnosis ~ cutoff_plasma + age), 
+                                    contrast_1 = "control", 
+                                    contrast_2 = "benign")
 
-pancrisk_just_plasma_model_2 <- fit(my_data_plasma, 
-                                    formula(diagnosis ~ cutoff_plasma + age), 
-                                    contrast = c("control", "malignant"))
+pancrisk_just_plasma_model_2 <- fit(data = my_data_plasma, 
+                                    form = formula(diagnosis ~ cutoff_plasma + age), 
+                                    contrast_1 = "control", 
+                                    contrast_2 = "malignant")
 
-pancrisk_logistic_1 <- fit(my_data_plasma, 
-                           formula(diagnosis ~ REG1B + LYVE1 + TFF1 + cutoff_plasma + age + creatinine), 
-                           contrast = c("control", "benign"))
+pancrisk_logistic_1 <- fit(data = my_data_plasma, 
+                           form = formula(diagnosis ~ REG1B + LYVE1 + TFF1 + cutoff_plasma + age + creatinine), 
+                           contrast_1 = "control", 
+                           contrast_2 = "benign")
 
-pancrisk_logistic_2 <- fit(my_data_plasma, 
-                           formula(diagnosis ~ REG1B + LYVE1 + TFF1 + cutoff_plasma + age + creatinine), 
-                           contrast = c("control", "malignant"))
-
-# pancrisk_model %>%
-#   tidy() %>%
-#   select(y.level, term, p.value) %>%
-#   group_by(y.level) %>%
-#   mutate(significant = case_when(p.value < 0.05 ~ TRUE,
-#                                  p.value >= 0.05 ~ FALSE))
-
-## Only plasma
+pancrisk_logistic_2 <- fit(data = my_data_plasma, 
+                           form = formula(diagnosis ~ REG1B + LYVE1 + TFF1 + cutoff_plasma + age + creatinine), 
+                           contrast_1 = "control", 
+                           contrast_2 = "malignant")
 
 my_data_val_just_plasma_1 <-
   my_data_plasma %>%
@@ -302,51 +310,77 @@ my_data_val_log_2 <-
   mutate(diagnosis = as.numeric(diagnosis)) %>%
   select(diagnosis, pred)
 
-roc1 <- pROC::roc(my_data_val_just_plasma_1$diagnosis, my_data_val_just_plasma_1$pred,
+roc1 <- my_data_val_just_plasma_1 %>%
+        pROC::roc(diagnosis, pred,
             plot = FALSE, conf.level = 0.95)
 
-roc2 <- pROC::roc(my_data_val_just_plasma_2$diagnosis, my_data_val_just_plasma_2$pred,
+roc2 <- my_data_val_just_plasma_2 %>%
+        pROC::roc(diagnosis, pred,
             plot = FALSE, conf.level = 0.95) 
 
-roc3 <- pROC::roc(my_data_val_simple_1$diagnosis, my_data_val_simple_1$pred,
+roc3 <- my_data_val_simple_1 %>%
+        pROC::roc(diagnosis, pred,
             plot = FALSE, conf.level = 0.95)
 
-roc4 <- pROC::roc(my_data_val_simple_2$diagnosis, my_data_val_simple_2$pred,
+roc4 <- my_data_val_simple_2 %>%
+        pROC::roc(diagnosis, pred,
             plot = FALSE, conf.level = 0.95) 
 
-roc5 <- pROC::roc(my_data_val_1$diagnosis, my_data_val_1$pred,
+roc5 <- my_data_val_1 %>%
+        pROC::roc(diagnosis, pred,
             plot = FALSE, conf.level = 0.95)
 
-roc6 <- pROC::roc(my_data_val_2$diagnosis, my_data_val_2$pred,
+roc6 <- my_data_val_2 %>%
+        pROC::roc(diagnosis, pred,
             plot = FALSE, conf.level = 0.95) 
 
-roc7 <- pROC::roc(my_data_val_plasma$diagnosis, my_data_val_plasma$pred,
+roc7 <- my_data_val_plasma %>%
+        pROC::roc(diagnosis, pred,
             plot = FALSE, conf.level = 0.95, levels = c(1, 2))
 
-roc8 <- pROC::roc(my_data_val_plasma$diagnosis, my_data_val_plasma$pred,
+roc8 <- my_data_val_plasma %>%
+        pROC::roc(diagnosis, pred,
             plot = FALSE, conf.level = 0.95, levels = c(2, 3)) 
 
-roc9 <- pROC::roc(my_data_val_log_1$diagnosis, my_data_val_log_1$pred,
+roc9 <- my_data_val_log_1 %>%
+        pROC::roc(diagnosis, pred,
             plot = FALSE, conf.level = 0.95)
 
-roc10 <- pROC::roc(my_data_val_log_2$diagnosis, my_data_val_log_2$pred,
+roc10 <- my_data_val_log_2 %>%
+         pROC::roc(diagnosis, pred,
             plot = FALSE, conf.level = 0.95) 
 
-my_data_val_just_plasma_1 <- find_cutoff(my_data_val_just_plasma_1, 1, 3)
+my_data_val_just_plasma_1 <- find_cutoff(data = my_data_val_just_plasma_1, 
+                                         const_1 = 1, 
+                                         const_2 = 3)
 
-my_data_val_just_plasma_2 <- find_cutoff(my_data_val_just_plasma_2, 2, 3)
+my_data_val_just_plasma_2 <- find_cutoff(data = my_data_val_just_plasma_2, 
+                                         const_1 = 2, 
+                                         const_2 = 3)
 
-my_data_val_simple_1 <- find_cutoff(my_data_val_simple_1, 1, 3)
+my_data_val_simple_1 <- find_cutoff(data = my_data_val_simple_1, 
+                                    const_1 = 1, 
+                                    const_2 = 3)
 
-my_data_val_simple_2 <- find_cutoff(my_data_val_simple_2, 2, 3)
+my_data_val_simple_2 <- find_cutoff(data = my_data_val_simple_2, 
+                                    const_1 = 2, 
+                                    const_2 = 3)
 
-my_data_val_1 <- find_cutoff(my_data_val_1, 1, 3)
+my_data_val_1 <- find_cutoff(data = my_data_val_1, 
+                             const_1 = 1, 
+                             const_2 = 3)
 
-my_data_val_2 <- find_cutoff(my_data_val_2, 2, 3)
+my_data_val_2 <- find_cutoff(data = my_data_val_2, 
+                             const_1 = 2, 
+                             const_2 = 3)
 
-my_data_val_log_1 <- find_cutoff(my_data_val_log_1, 1, 3)
+my_data_val_log_1 <- find_cutoff(data = my_data_val_log_1, 
+                                 const_1 = 1, 
+                                 const_2 = 3)
 
-my_data_val_log_2 <- find_cutoff(my_data_val_log_2, 2, 3)
+my_data_val_log_2 <- find_cutoff(data = my_data_val_log_2, 
+                                 const_1 = 2, 
+                                 const_2 = 3)
 
 acc_1 <- 
   print_acc(data_1 = table(my_data_val_just_plasma_1$pred, my_data_val_just_plasma_1$diagnosis) %>%
@@ -354,6 +388,7 @@ acc_1 <-
             data_2 = table(my_data_val_just_plasma_2$pred, my_data_val_just_plasma_2$diagnosis) %>%
               confusionMatrix(),
             title = "Binary CA19_9")
+
 acc_2 <- 
   print_acc(data_1 = table(my_data_val_simple_1$pred, my_data_val_simple_1$diagnosis) %>%
               confusionMatrix(),
@@ -367,7 +402,6 @@ acc_3 <-
             data_2 = table(my_data_val_2$pred, my_data_val_2$diagnosis) %>%
               confusionMatrix(),
             title = "PancRISK without Plasma")
-  
 
 acc_4 <- 
   print_acc(data_1 = table(my_data_val_log_1$pred, my_data_val_log_1$diagnosis) %>%
@@ -375,9 +409,3 @@ acc_4 <-
       data_2 = table(my_data_val_log_2$pred, my_data_val_log_2$diagnosis) %>%
       confusionMatrix(),
     title = "PancRISK (Sep. regression)")
-
-# acc_5 <- 
-#   table(my_data_val_log_2$pred, my_data_val_log_2$diagnosis) %>%
-#   confusionMatrix() %>%
-#   rename_at(c(2, 3), ~ (c(1, 2))) %>% 
-#   print_acc(title = "PancRISK - seperate logistic regression")
