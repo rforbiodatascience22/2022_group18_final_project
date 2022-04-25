17d463471b9bc5f4f0f37cbaf6e00a43cd793cbf
pokepup <s213012@dtu.dk>
2022-04-25 09:34:00 +0000

    Tidying p2

--- PancRISK_project/R/02_clean.R
+++ PancRISK_project/R/02_clean.R
@@ -17,7 +17,9 @@ my_data_clean <- my_data %>%
                                       sample_origin == "UCL" ~ "United Kingdom",
                                       sample_origin == "LIV" ~ "United Kingdom",
                                       sample_origin == "ESP" ~ "Spain"),
-         sample_id = gsub('^.', '', sample_id)) %>% 
+         sample_id = gsub(pattern = '^.', 
+                          replacement = '', 
+                          x = sample_id)) %>% 
   select(.,
          -sample_origin,
          -benign_sample_diagnosis)
--- PancRISK_project/R/99_proj_functions.R
+++ PancRISK_project/R/99_proj_functions.R
@@ -1,15 +1,27 @@
-validate <- function(data, model, diagnosis_1, diagnosis_2) {
+# Validate ---------------------------------------------------------------------
+validate <- function(data, 
+                     model, 
+                     diagnosis_1, 
+                     diagnosis_2) {
   
 }
-
-find_cutoff <- function(data, const_1, const_2) {
-  cutoff_val <- cutoff::roc(data$pred, data$diagnosis)$cutoff
+# Cutoff -----------------------------------------------------------------------
+find_cutoff <- function(data, 
+                        const_1, 
+                        const_2) {
+  cutoff_val <- cutoff::roc(data$pred, 
+                            data$diagnosis)$cutoff
   
   data %>%
-    mutate(pred = ifelse(pred < cutoff_val, const_1, const_2))
+    mutate(pred = ifelse(pred < cutoff_val, 
+                         const_1, 
+                         const_2))
 }
-
-fit <- function(data, form, contrast_1, contrast_2) {
+# Fit --------------------------------------------------------------------------
+fit <- function(data, 
+                form, 
+                contrast_1, 
+                contrast_2) {
   data %>%
     filter(diagnosis == contrast_1 | diagnosis == contrast_2) %>%
     glm(formula = form,
@@ -17,48 +29,86 @@ fit <- function(data, form, contrast_1, contrast_2) {
         na.action = na.omit,
         family = binomial(link = "logit"))
 }
-
-print_acc <- function(data_1, data_2, title = "Group") {
+# Accuracy ---------------------------------------------------------------------
+print_acc <- function(data_1, 
+                      data_2, 
+                      title = "Group") {
   accurancy_1 <- 
     data_1 %>%
     tidy() %>%
     filter(term == "accuracy") %>%
-    mutate(result = paste(round(estimate, 3), " 95% CI: ", round(conf.low, 3), "-", round(conf.high, 3))) %>%
+    mutate(result = paste(round(estimate, 
+                                digits = 3), 
+                          " 95% CI: ", 
+                          round(conf.low, 
+                                digits = 3), 
+                          "-", 
+                          round(conf.high,
+                                digits = 3))) %>%
     select(result) %>%
     pull()
+  
   accurancy_2 <- 
     data_1 %>%
     tidy() %>%
     filter(term == "accuracy") %>%
-    mutate(result = paste(round(estimate, 3), " 95% CI: ", round(conf.low, 3), "-", round(conf.high, 3))) %>%
+    mutate(result = paste(round(estimate, 
+                                digits = 3), 
+                          " 95% CI: ", 
+                          round(conf.low, 
+                                digits = 3), 
+                          "-", 
+                          round(conf.high, 
+                                digits = 3))) %>%
     select(result) %>%
     pull()
+  
   table_1 <-
     data_1 %>%
     tidy() %>%
     pivot_wider(values_from = estimate, 
                 names_from = class) %>%
     filter(term == "sensitivity" | term == "specificity") %>%
-    select(c(term, '1')) %>%
+    select(c(term, 
+             '1')) %>%
     mutate_at(c('1'), round, 3) %>%
-    mutate(term = c("Sensitivity", "Specificity")) 
+    mutate(term = c("Sensitivity", 
+                    "Specificity")) 
   
   data_2 %>%
     tidy() %>%
     pivot_wider(values_from = estimate, 
                 names_from = class) %>%
     filter(term == "sensitivity" | term == "specificity") %>%
-    select(c(term, '2')) %>%
-    mutate_at(c('2'), round, 3) %>%
-    mutate(term = c("Sensitivity", "Specificity")) %>%
+    select(c(term, 
+             '2')) %>%
+    mutate_at(c('2'), 
+              round, 
+              3) %>%
+    mutate(term = c("Sensitivity", 
+                    "Specificity")) %>%
     full_join(table_1) %>%
-    knitr::kable(align = c(rep('c', times = 3)), 
-                 col.names = NULL,
-    ) %>%
+    knitr::kable(align = c(rep('c', 
+                               times = 3)), 
+                 col.names = NULL,) %>%
     #add_header_above(c("Benign vs. Malignant" = 1, " " = 1)) %>% 
-    add_header_above(c("\ \n\n \ " = 1, setNames(1, "Control vs. Malignant"), setNames(1, "Benign vs. Malignant")), line = TRUE, bold = FALSE) %>%
-    add_header_above(c("Accurancy\n\n \ " = 1, setNames(1, accurancy_1), setNames(1, accurancy_2)), line = TRUE, bold = FALSE) %>%
-    add_header_above(c(setNames(3, paste("Performance - ", title)))) %>% 
+    add_header_above(c("\ \n\n \ " = 1, 
+                       setNames(object = 1, 
+                                nm = "Control vs. Malignant"), 
+                       setNames(object = 1, 
+                                nm = "Benign vs. Malignant")), 
+                     line = TRUE, 
+                     bold = FALSE) %>%
+    add_header_above(c("Accurancy\n\n \ " = 1, 
+                       setNames(object = 1, 
+                                nm = accurancy_1), 
+                       setNames(object = 1, 
+                                nm = accurancy_2)), 
+                     line = TRUE, 
+                     bold = FALSE) %>%
+    add_header_above(c(setNames(object = 3, 
+                                nm = paste("Performance - ", 
+                                           title)))) %>% 
     kable_styling(position = "center") %>%
     column_spec (1:3, 
                  border_left = "1px solid #ddd;", 
@@ -70,19 +120,23 @@ print_acc <- function(data_1, data_2, title = "Group") {
                 width = "6em")
 }
 
-plot_roc <- function(roc_render_1, roc_render_2, title = "ROC", caption) {
+# ROC --------------------------------------------------------------------------
+plot_roc <- function(roc_render_1, 
+                     roc_render_2, 
+                     title = "ROC", 
+                     caption) {
   plotx_1 <- rev(roc_render_1$specificities)
   ploty_1 <- rev(roc_render_1$sensitivities)
   auc_1 <- roc_render_1$auc
   auc_ci_1 <- roc_render_1 %>%
     ci.auc() %>%
-    round(3)
+    round(digits = 3)
   plotx_2 <- rev(roc_render_2$specificities)
   ploty_2 <- rev(roc_render_2$sensitivities)
   auc_2 <- roc_render_2$auc
   auc_ci_2 <- roc_render_2 %>%
     ci.auc() %>%
-    round(3)
+    round(digits = 3)
   ggplot(NULL) +
     geom_segment(aes(x = 0, 
                      y = 1, 
@@ -96,9 +150,19 @@ plot_roc <- function(roc_render_1, roc_render_2, title = "ROC", caption) {
                   color = "high")) +
     scale_color_discrete(name="Contrast",
                          labels=c(paste0("Control vs. Benign\n",
-                                         round(auc_1, 3), ", 95% CI: ", auc_ci_1[1], "-", auc_ci_1[3]), 
+                                         round(auc_1, 
+                                               digits = 3), 
+                                         ", 95% CI: ", 
+                                         auc_ci_1[1], 
+                                         "-", 
+                                         auc_ci_1[3]), 
                                   paste0("Benign vs. Malignant\n",
-                                         round(auc_2, 3), ", 95% CI: ", auc_ci_2[1], "-", auc_ci_2[3])),
+                                         round(auc_2, 
+                                               digits = 3), 
+                                         ", 95% CI: ", 
+                                         auc_ci_2[1], 
+                                         "-", 
+                                         auc_ci_2[3])),
                          breaks=c("low", "high")) +
     scale_x_reverse(name = "Specificity",
                     limits = c(1, 0), 
