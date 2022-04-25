4b08be9410ea4bc24262646d4588b9aa8fb80884
Yassine2612 <s211972@dtu.dk>
2022-04-25 08:25:21 +0000

    Before pulling

--- PancRISK_project/R/00_doit.R
+++ PancRISK_project/R/00_doit.R
@@ -1,10 +1,5 @@
 # Run all scripts ---------------------------------------------------------
-set.seed(123)
 source(file = "PancRISK_project/R/01_load.R")
 source(file = "PancRISK_project/R/02_clean.R")
 source(file = "PancRISK_project/R/03_augment.R")
 source(file = "PancRISK_project/R/04_model_i.R")
-
-rmarkdown::render(input = "PancRISK_project/Documents/trial_presentation_slide.Rmd", 
-     output_file = "PancRISK_project/Documents/trial_presentation_slide.html",
-     output_dir = "PancRISK_project/Documents/")
