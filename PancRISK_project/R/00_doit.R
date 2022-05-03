cc8481a8c79811e5882a629c721c22927a6215d3
belkischau <belkischau@hotmail.com>
2022-05-03 21:25:37 +0000

    Add 2 data visualization slides

--- PancRISK_project/R/00_doit.R
+++ PancRISK_project/R/00_doit.R
@@ -18,6 +18,7 @@ source(file = "PancRISK_project/R/99_proj_functions.R")
 # Run all scripts ---------------------------------------------------------
 source(file = "PancRISK_project/R/01_load.R")
 source(file = "PancRISK_project/R/02_clean.R")
+source(file = "PancRISK_project/R/025_data_visualization.R")
 source(file = "PancRISK_project/R/03_augment.R")
 source(file = "PancRISK_project/R/04_model_i.R")
 source(file = "PancRISK_project/R/05_model_ii.R")
