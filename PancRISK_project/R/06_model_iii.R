d3e3c05a16caff848d1bdd8cc5bb1eeea0f50284
Yassine2612 <s211972@dtu.dk>
2022-05-05 15:22:58 +0000

    Fixing some errors while running source

--- PancRISK_project/R/06_model_iii.R
+++ PancRISK_project/R/06_model_iii.R
@@ -1,167 +1,3 @@
-# Wrangle data -----------------------------------------------------------------
-
-PCA <- my_data_clean_PCA %>% 
-  filter(diagnosis != 0) %>%
-  select(patient_cohort,
-         age,
-         sex, 
-         stage ,
-         plasma_CA19_9,
-         creatinine, 
-         LYVE1, 
-         REG1B, 
-         TFF1)
-
-# Model data -------------------------------------------------------------------
-
-pca_fit_ <- PCA  %>%
-  select(where(is.numeric)) %>%
-  prcomp(scale = TRUE)
-
-
-data_pca <- pca_fit_ %>% 
-  augment(PCA)
-
-
-# Here i'm testing how the 3 biomarkers can allow a better distinction between the malignant samples and the benign ones 
-
-k_means_PC_1_2 <- data_pca %>%
-  select(.fittedPC1, .fittedPC2)  %>%
-  kmeans(centers = 2) %>%
-  augment(data_pca) %>%
-  rename(clusters_pca = .cluster)%>%
-  mutate(clusters_pca = case_when(
-    clusters_pca == 1 ~ "1",
-    clusters_pca == 2 ~ "2"),
-    diagnosis = case_when(
-      diagnosis == 1 ~ "benign",
-      diagnosis == 2 ~ "malignant"))
-
-
-
-## Model including only plasma_CA19_9
-
-plasma_CA19_9_alone <- k_means_PC_1_2 %>%
-  select(patient_cohort,age,sex,plasma_CA19_9 ) %>%
-  kmeans(centers = 2)%>%
-  augment(k_means_PC_1_2) %>% 
-  rename(clusters = .cluster)
-
-
-
-plasma_only_evaluation <- plasma_CA19_9_alone %>% 
-  as_tibble %>% 
-  bind_cols%>%
-  mutate(eval = case_when(
-    clusters == 1 & diagnosis == "malignant"~ "TP",
-    clusters == 2 & diagnosis == "benign" ~ "TN",
-    clusters == 1 & diagnosis == "benign" ~ "FP",
-    clusters == 2 & diagnosis == "malignant" ~ "FN"))%>% 
-  group_by(eval)%>%
-  summarise(count = n())
-
-plasma_only_evaluation
-## Model including only LYVE1, REG1B, TFF1 biomarkers
-
-
-biomarkers_only <- k_means_PC_1_2 %>%
-  select( patient_cohort,age,sex, LYVE1, REG1B, TFF1)  %>%
-  kmeans(centers = 2)%>%
-  augment(k_means_PC_1_2) %>% 
-  rename(clusters= .cluster)
-
-
-biomarkers_only_evaluation <- biomarkers_only %>% 
-  as_tibble %>% 
-  bind_cols%>%
-  mutate(eval = case_when(
-    clusters == 2 & diagnosis == "malignant"~ "TP",
-    clusters == 1 & diagnosis == "benign" ~ "TN",
-    clusters == 2 & diagnosis == "benign" ~ "FP",
-    clusters == 1 & diagnosis == "malignant" ~ "FN"))%>% 
-  group_by(eval)%>%
-  summarise(count = n())
-  
-View(biomarkers_only_evaluation)
-
-## Full model including plasma_CA19_9 + creatinine + LYVE1 + REG1B + TFF1 
-
-complete_model <- k_means_PC_1_2 %>%
-  select( patient_cohort,age,sex, LYVE1, REG1B, TFF1,plasma_CA19_9)  %>%
-  kmeans(centers = 2)%>%
-  augment(k_means_PC_1_2) %>% 
-  rename(clusters = .cluster)
-
-
-plasma_CA19_9_alone <- plasma_CA19_9_alone %>%  
-  mutate(clusters = as.factor(clusters),
-         diagnosis = as.factor(diagnosis))
-
-
-plasma_CA19_9_plot <- ggplot() +
-  geom_point( data= plasma_CA19_9_alone , aes(x = .fittedPC1, y = .fittedPC2 , color = diagnosis , size = 1.5  , alpha=1/1000) ) +
-  geom_point( data = plasma_CA19_9_alone , aes(x = .fittedPC1, y = .fittedPC2 , color = clusters) ,size = 1   ) +
-  scale_color_manual(values=c("blue", "red","red", "blue" ))+
-  labs( title = "plasma_CA19_91") +
-  theme_minimal(base_size = 10,
-                base_family = "Avenir")+
-  theme(plot.title = element_text(size=8))+
-  theme_minimal(8)
-
-plasma_CA19_9_plot<-plasma_CA19_9_plot+
-  guides(size='none',
-         alpha='none')+
-  theme(legend.position = "bottom")
-
-
-biomarkers <- biomarkers_only %>%  
-  mutate(clusters = as.factor(clusters),
-         diagnosis = as.factor(diagnosis))
-
-  biomarkers_plot <- ggplot() +
-  geom_point( data= biomarkers , aes(x = .fittedPC1, y = .fittedPC2 , color = diagnosis , size = 1.5  , alpha=1/1000) ) +
-  geom_point( data = biomarkers , aes(x = .fittedPC1, y = .fittedPC2 , color = clusters) ,size = 1   ) +
-  scale_color_manual(values=c("red", "blue","red", "blue" ))+
-  labs( title = "LYVE1+REG1B+TFF1") +
-  theme_minimal(base_size = 10,
-                base_family = "Avenir")+
-  theme(plot.title = element_text(size=8))+
-  theme_minimal(8)
-
-  biomarkers_plot<-biomarkers_plot+
-  guides(size='none',
-         alpha='none')+
-  theme(legend.position = "bottom")
-
-  
-  complete_model <- complete_model %>%  
-    mutate(clusters = as.factor(clusters),
-           diagnosis = as.factor(diagnosis))
-
-  complete_modelplot<- ggplot() +
-    geom_point( data= complete_model , aes(x = .fittedPC1, y = .fittedPC2 , color = diagnosis , size = 1.5  , alpha=1/1000) ) +
-    geom_point( data = complete_model , aes(x = .fittedPC1, y = .fittedPC2 , color = clusters) ,size = 1   ) +
-    scale_color_manual(values=c("blue", "red","blue", "red" ))+
-    labs( title = "LYVE1+REG1B+TFF1+plasma_CA19_9") +
-    theme_minimal(base_size = 10,
-                  base_family = "Avenir")+
-    theme(plot.title = element_text(size=8))+
-    theme_minimal(8)
-  
-  complete_modelplot<-complete_modelplot+
-    guides(size='none',
-           alpha='none')+
-    theme(legend.position = "bottom")
-  
-patchwork_diag <-  (plasma_CA19_9_plot |biomarkers_plot)+ 
-  plot_layout(guides = 'collect') 
-patchwork_diag + plot_annotation(
-  title = "K-means results " ,
-  caption= "Data from Silvana Debernardi et al" )&
-  theme(legend.position = "bottom")
-
-
-
 data_aug_pca <- pca_fit %>% 
   augment(my_data_clean_PCA)
 
@@ -169,13 +5,9 @@ malignant_data_aug_pca <- malignant_PCA_fit %>%
   augment(malignant_PCA)
 
 
-
-
 # Model data -------------------------------------------------------------------
 
-
-
-Kmeans_blood_diagnosis <- data_aug_pca %>%
+Kmeans_blood_diagnosis_ <- data_aug_pca %>%
   select(patient_cohort,
          age,
          sex,
@@ -188,7 +20,7 @@ Kmeans_blood_diagnosis <- data_aug_pca %>%
                                 diagnosis == 1 ~ "benign",
                                 diagnosis == 2 ~ "malignant"))
 
-Kmeans_urinary_diagnosis <- data_aug_pca %>%
+Kmeans_urinary_diagnosis_ <- data_aug_pca %>%
   select(patient_cohort,
          age,
          sex, 
@@ -208,13 +40,13 @@ Kmeans_urinary_diagnosis <- data_aug_pca %>%
 # How plasma CA19_9 (blood diagnosis ) can discriminate between control benign and malignant individuals
 
 
-Kmeans_blood_diagnosis_control <-Kmeans_blood_diagnosis %>% 
+Kmeans_blood_diagnosis_control <-Kmeans_blood_diagnosis_ %>% 
   filter(diagnosis=="control")
 
-Kmeans_blood_diagnosis_benign <-Kmeans_blood_diagnosis %>% 
+Kmeans_blood_diagnosis_benign <-Kmeans_blood_diagnosis_ %>% 
   filter(diagnosis=="benign")
 
-Kmeans_blood_diagnosis__malignant <-Kmeans_blood_diagnosis %>% 
+Kmeans_blood_diagnosis__malignant <-Kmeans_blood_diagnosis_ %>% 
   filter(diagnosis=="malignant")
 
 blood_diagnosis_plot <- ggplot() +
@@ -235,13 +67,13 @@ blood_diagnosis_plot<-blood_diagnosis_plot+
 # How the 3 biomarkers (urinary diagnosis ) can discriminate between control benign and malignant individuals
 
 
-Kmeans_urinary_diagnosis_control <-Kmeans_urinary_diagnosis %>% 
+Kmeans_urinary_diagnosis_control <-Kmeans_urinary_diagnosis_ %>% 
   filter(diagnosis=="control")
 
-Kmeans_urinary_diagnosis_benign <- Kmeans_urinary_diagnosis %>% 
+Kmeans_urinary_diagnosis_benign <- Kmeans_urinary_diagnosis_ %>% 
   filter(diagnosis=="benign")
 
-Kmeans_urinary_diagnosis_malignant <- Kmeans_urinary_diagnosis %>% 
+Kmeans_urinary_diagnosis_malignant <- Kmeans_urinary_diagnosis_ %>% 
   filter(diagnosis=="malignant")
 
 urinary_diagnosis_plot <- ggplot() +
@@ -260,8 +92,8 @@ urinary_diagnosis_plot<-urinary_diagnosis_plot+
   theme(legend.position = "bottom")
 
 
-kmeans_plot <- ( blood_diagnosis_plot | urinary_diagnosis_plot)
-kmeans_plot <- kmeans_plot + plot_annotation(title = "Comparison between Blood vs Urinary biomarkers in classification of diagnosis and cancer stages",
+kmeans_plot_ <- ( blood_diagnosis_plot | urinary_diagnosis_plot)
+kmeans_plot_ <- kmeans_plot_ + plot_annotation(title = "Comparison between Blood vs Urinary biomarkers in classification of diagnosis and cancer stages",
                                                          caption = "Data from Silvana Debernardi et al")
 
 
